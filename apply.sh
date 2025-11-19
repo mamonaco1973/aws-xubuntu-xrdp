42738503a5bb1518e4bcda80d2245c6549b14014
Mike Monaco <mamonaco1973@gmail.com>
2025-11-19 14:06:35 -0500

    build xubuntu ami

--- apply.sh
+++ apply.sh
@@ -72,7 +72,7 @@ vpc_id=$(aws ec2 describe-vpcs \
   --output text)
 
 subnet_id=$(aws ec2 describe-subnets \
-  --filters "Name=tag:Name,Values=pub-subnet-1" \
+  --filters "Name=tag:Name,Values=vm-subnet-1" \
   --query "Subnets[0].SubnetId" \
   --output text)
 
