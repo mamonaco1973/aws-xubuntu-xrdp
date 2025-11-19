133e63e2f862b10be71d3ca2a4a05c87c58441d9
Mike Monaco <mamonaco1973@gmail.com>
2025-11-19 14:20:56 -0500

    turn off snap first

--- 02-packer/packages.sh
+++ 02-packer/packages.sh
@@ -20,11 +20,11 @@ sudo systemctl stop snap.amazon-ssm-agent.amazon-ssm-agent.service || true
 sudo snap remove --purge amazon-ssm-agent
 sudo snap remove --purge core22
 sudo snap remove --purge snapd
-sudo apt purge -y snapd
-sudo apt autoremove --purge -y
+sudo apt-get purge -y snapd
+sudo apt-get autoremove --purge -y
 echo -e "Package: snapd\nPin: release *\nPin-Priority: -10" \
  | sudo tee /etc/apt/preferences.d/nosnap.pref
-sudo apt update
+sudo apt-get update -y
 curl https://s3.amazonaws.com/amazon-ssm-us-east-1/latest/debian_amd64/amazon-ssm-agent.deb -o ssm.deb
 sudo dpkg -i ssm.deb
 systemctl enable amazon-ssm-agent
