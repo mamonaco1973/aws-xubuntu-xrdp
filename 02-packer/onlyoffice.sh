3c24785b2697fa2bd911694583db83e4b2d0cdc4
Mike Monaco <mamonaco1973@gmail.com>
2025-11-22 17:49:10 -0500

    fix wget status

--- 02-packer/onlyoffice.sh
+++ 02-packer/onlyoffice.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 cd /tmp
-wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb
+wget -q https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb
 apt-get install -y ./onlyoffice-desktopeditors_amd64.deb
 rm onlyoffice-desktopeditors_amd64.deb
 
