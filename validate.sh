d4963af1bc13881ad80bf470a0ef9c0c5f361d15
Mike Monaco <mamonaco1973@gmail.com>
2025-11-23 17:54:16 -0500

    fix validate script

--- validate.sh
+++ validate.sh
@@ -28,7 +28,7 @@ windows_dns=$(aws ec2 describe-instances \
 if [ -z "$windows_dns" ]; then
   echo "WARN: No Windows AD instance found with tag Name=windows-ad-admin"
 else
-  echo "NOTE: Windows Instance DNS: $(echo $windows_dns | xargs)"
+  echo "NOTE: Windows Instance FQDN: $(echo $windows_dns | xargs)"
 fi
 
 # --------------------------------------------------------------------------------------------------
@@ -42,5 +42,5 @@ linux_dns=$(aws ec2 describe-instances \
 if [ -z "$linux_dns" ]; then
   echo "WARN: No Linux AD instance found with tag Name=xubuntu-instance"
 else
-  echo "NOTE: Xubuntu Desktop Instance DNS:   $(echo $linux_dns | xargs)"
+  echo "NOTE: Xubuntu Desktop Instance FQDN: $(echo $linux_dns | xargs)"
 fi
